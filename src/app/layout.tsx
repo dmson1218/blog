926ce16a3a3cb2161dc1e7bde65fe1e8e9a587bc
dmson1218 <dmson1218@naver.com>
2024-06-13 11:02:27 +0900

    feat: 헤더 컴포넌트 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Navigation from '#components/Navigation';
 
 export const metadata: Metadata = {
     title: 'ForFree Blog',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
     return (
         <html>
-            <body>{children}</body>
+            <body>
+                <Navigation />
+                {children}
+            </body>
         </html>
     );
 }
