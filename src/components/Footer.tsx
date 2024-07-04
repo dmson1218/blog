8bc3de4bb7866b70bd6e3375243266d1399db793
dmson1218 <dmson1218@naver.com>
2024-07-04 22:34:22 +0900

    feat: footer 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer = () => (
+    <footer className="py-2 border-t border-black">
+        <div className="text-center text-sm">
+            © 2024 Dongmin Son. All rights reserved.
+        </div>
+        <div className="flex justify-center gap-2">
+            <a href="mailto:dmson1218@naver.com" className="text-sm">
+                Email.
+            </a>
+            <a href="https://github.com/dmson1218" className="text-sm">
+                GitHub.
+            </a>
+            <a
+                href="https://www.instagram.com/for_free_1218"
+                className="text-sm"
+            >
+                Instagram.
+            </a>
+        </div>
+    </footer>
+);
+
+export default Footer;
