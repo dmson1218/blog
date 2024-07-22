c3373573ba08a6d2a092a7136269dfd963697e5a
dmson1218 <dmson1218@naver.com>
2024-07-22 15:20:49 +0900

    fix: 타입 오류 수정

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,6 @@ const Head = ({ title, category, date }: MetaData) => {
     return (
         <head>
             <title>{title}</title>
-            <meta name="description" content={category} />
             <meta name="date" content={date.toISOString()} />
         </head>
     );
