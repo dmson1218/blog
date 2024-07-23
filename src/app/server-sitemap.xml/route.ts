7edc6c37a360984d44a0d22a789de7e84c28c5ae
dmson1218 <dmson1218@naver.com>
2024-07-23 21:36:14 +0900

    feat: 서버 사이드 사이트맵 추가

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -1,9 +1,10 @@
 // app/server-sitemap.xml/route.ts
 import { getServerSideSitemap } from 'next-sitemap';
-import { getMDFiles } from '../../utils/markdown';
+import { getMDFiles, getTags } from '../../utils/markdown';
 
 export async function GET(request: Request) {
     const metaDatas = getMDFiles();
+    const tags = getTags();
 
     return getServerSideSitemap([
         {
@@ -15,6 +16,11 @@ export async function GET(request: Request) {
         ...metaDatas.map(metaData => ({
             loc: `https://dmson1218.com/posts/${metaData.title.replace(/ /g, '%20')}`,
             lastmod: metaData.date.toISOString(),
+            priority: 0.7,
+        })),
+        ...tags.map(tag => ({
+            loc: `https://dmson1218.com/tags/${tag.replace(/ /g, '%20')}`,
+            lastmod: new Date().toISOString(),
             priority: 0.7,
         })),
     ]);
